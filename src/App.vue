482fdb4fd17a3c7b3886b46258d8f547f2b74330
MNurlybekuly <mnurlubekuly@smartex.kz>
2023-09-23 18:06:37 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <BrokerList />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import BrokerList from '@/components/BrokerList.vue'
+</script>
---
+++ src/components/BrokerList.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center fill-height">
+      {{ store.state.count }}
+      <v-list lines="two">
+        <v-list-item v-for="n in 3" :key="n" :title="'Item ' + n"
+          subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"></v-list-item>
+      </v-list>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  import { useStore } from '@/plugins/store';
+  const store = useStore();
+  store.commit('increment')
+</script>
